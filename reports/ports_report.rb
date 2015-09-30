291783edfa0dfd72cc2050182b80ba0dee21af6a
dataking <dataking@gmail.com>
2015-09-30 13:05:33 -0700

    Rough ports report done.  Prints to PDF but isn't pretty.

--- reports/ports_report.rb
+++ reports/ports_report.rb
@@ -5,16 +5,30 @@ require 'colorize'
 require 'sqlite3'
 require 'getoptlong'
 require 'pp'
+require 'pdfkit'
+
+def humanize_uptime(uptime)
+	[[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
+		if uptime.to_i > 0
+			secs, n = uptime.to_i.divmod(count)
+			"#{n.to_i} #{name}"
+		end
+	}.compact.reverse.join(' ')
+end
 
 opts = GetoptLong.new(
 	['--database', '-d', GetoptLong::OPTIONAL_ARGUMENT ],
+	['--output', '-o', GetoptLong::REQUIRED_ARGUMENT ],
 )
 
 database = ''
+outputpdf = '/tmp/ports_report.pdf'
 opts.each do |opt,arg|
 	case opt
 	when '--database'
 		database = arg
+	when '--output'
+		outputpdf = arg
 	end
 end
 
@@ -37,29 +51,56 @@ ports.each do |p|
 	end
 end	
 
-pp hostsByPort
-puts <<-EOS
-
-<html>
+#pp hostsByPort
+html = "<html>
 	<head>
 		<title>Ports report</title>
 	</head>
 	<body>
-		<div id="main">
-			<table border="1">
-EOS
-hostsByPort.each do |p|
-	pp p
-	puts "\t\t\t\t<tr><td colspan=\"2\"><h3><b>#{p}</b></h3></td></tr>"
+		<h1 id=\"page_title\">Ports Report: Hosts by open port</h1>
+		<div id=\"main\">
+			<table border=\"1\">\n"
+hostsByPort.each_key do |p|
+	#pp p
+	html = html + "\t\t\t\t<tr><td colspan=\"2\"><h3><b>#{p}</b></h3></td></tr>\n"
 	hostsByPort[p].each do |h|
 		r = db.execute("SELECT ip4,hostname FROM hosts WHERE hid='#{h}'")
-		puts "\t\t\t\t<tr><td>#{pp r}</td><td>&nbsp;</td></tr>"
+		r.flatten!
+		html = html + "\t\t\t\t<tr><td><a href=\"##{r[0]}\">#{r[0]}</a></td><td>#{r[1]}</td></tr>"
 	end
 end
-puts <<-EOS
-			</table>
+html = html + "\t\t\t</table>
 		</div>
+		<div id=\"hosts\">
+			<table border=\"1\">
+				<tr><th>IPv4 Address</th><th>Hostname</th><th>Status</th><th># TCP Ports</th><th># UDP Ports</th><th>MAC Address</th><th>MAC Vendor</th><th>IPv6 Address</th><th>Hop Distance</th><th>Uptime</th><th>Last Reboot</th></tr>\n"
+db.execute("SELECT ip4,hostname,status,tcpcount,udpcount,mac,vendor,ip6,distance,uptime,upstr FROM hosts") do |rec|
+	#pp rec
+	html = html + "\t\t\t\t<tr>"
+	i = 0
+	rec.each do |e|
+		if i == 9
+			html = html + "<td>" + humanize_uptime(e) + "</td>"
+		else
+			if i == 0
+				html = html + "<td><a name=\"#{e}\">#{e}</a></td>"
+			else
+				html = html + "<td>#{e}</td>"
+			end
+		end
+		i += 1
+	end
+	html = html + "</tr>"
+end
+html = html + "\t\t\t</table>
+		</div>			
 	</body>
-</html>
-EOS
+</html>"
+
+#puts html
+kit = PDFKit.new(html)
+#kit.stylesheets << '/path/to/pdf.css'
+kit.to_pdf
+kit.to_file(outputpdf)
+puts "File saved to #{outputpdf}."
 
